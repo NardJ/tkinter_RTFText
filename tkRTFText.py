5b0274b6d501adc2b6a3e91a7c882bbc9e417002
Nard Janssens <nardjanssens@gmail.com>
2021-02-14 16:16:45 +0100

    Add files via upload

--- tkRTFText.py
+++ tkRTFText.py
@@ -113,22 +113,30 @@ class RTFText(tk.Text):
         return str(self.frame)
 
 if __name__=="__main__":
+    # Create window
     root=tk.Tk()   
     root.configure(background='white')
 
+    # Add widget RTFText
     RTF=RTFText(root,bg='white')#root,bg="white")
     RTF.pack(side=tk.TOP, fill=tk.BOTH,padx=(0,0),pady=(2,2))
 
-    rtf=("<family:verdana>Verdana <size:18>18pt</size> <size:14>14pt</size></family>\n"+
-        "<color:red>red</color> <background:red>red</background>\n"+
-        "<H1>Header </H1>  <H2>Header </H2>   <H3>Header </H3>   <H4>Header </H4>\n"+
-        "<b>Bold</b>  <i>Italic</i>   <u>Underscore</u>\n"+
-        "<b>Bold <i> italic</i> </b>\n"+
-        "<center> centered </center>\n"+
+    # Make some rtf/'html' to display
+    rtf=("<family:verdana>Verdana <size:14>14pt</size> <size:18>18pt</size></family>\n"+
+        "<color:red>red text</color>\n<background:yellow>yellow background</background>\n"+
+        "<H1>Header</H1>\n<H2>Header</H2>\n<H3>Header</H3>\n<H4>Header </H4>\n"+
+        "<b>Bold</b>\n<i>Italic</i>\n<u>Underscore</u>\n"+
+        "<b>Bold <i>and italic</i> </b>\n"+
+        "<center>Text can also be centered </center>\n"+
+        "Page divider below:\n"+
         "<hr>\n"+
-        "</hr>\n")    
-    # these should also be settable with brackets<>, but not yet implemented
+        "</hr>\n"+
+        "Page divider above")    
+
+    # Set rtf to widget
     RTF.setRTF(rtf,pad=(8,8),bg='white', font=tkf.Font(family='Terminal', weight = 'normal', size = 9))
+    
+    # Display window and wait user actions
     root.mainloop()
 
  
